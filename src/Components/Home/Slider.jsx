3189c4456b211aa07b569bc78031265f62e287a0
ShanaxRules <shahnawazdeveloper21@gmail.com>
2024-12-25 02:35:57 +0600

    added dark overlay

--- src/Components/Home/Slider.jsx
+++ src/Components/Home/Slider.jsx
@@ -21,6 +21,7 @@ const Slider = () => {
                     <img
                         src="https://i.ibb.co.com/4W8YLSN/625b10a58137b364b18df2ea-i-Stock-94179607.jpg"
                         className="w-full" />
+                        <div className="absolute inset-0 bg-black opacity-50"></div>
                     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                         <a onClick={()=>navigateCarousel(3)} className="btn btn-circle">❮</a>
                         <div className="flex flex-col lg:gap-y-8 md:gap-y-6 gap-y-2 text-center">
@@ -38,6 +39,7 @@ const Slider = () => {
                     <img
                         src="https://i.ibb.co.com/nDvVhmg/istockphoto-899471458-612x612.jpg"
                         className="w-full" />
+                        <div className="absolute inset-0 bg-black opacity-50"></div>
                     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-start">
                         <a onClick={()=>navigateCarousel(1)}  className="btn btn-circle mr-3">❮</a>
                         <div className="flex flex-col lg:gap-y-8 md:gap-y-6 gap-y-2 text-left flex-1">
@@ -56,6 +58,7 @@ const Slider = () => {
                     <img
                         src="https://i.ibb.co.com/gmdXDNj/depositphotos-21515189-stock-photo-agent-with-house-model-and.webp"
                         className="w-full" />
+                        <div className="absolute inset-0 bg-black opacity-50"></div>
                     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-end">
                         <a onClick={()=>navigateCarousel(2)}  className="btn btn-circle">❮</a>
                         <div className="flex flex-col lg:gap-y-8 md:gap-y-6 gap-y-2 text-right flex-1">
